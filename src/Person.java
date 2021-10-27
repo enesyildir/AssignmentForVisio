cfeb50536257517eed58411e375869ba02a2f34d
Enes Yildir <ahmed.yildir@ngc.com>
2021-10-27 09:48:16 -0400

    assignment first commit

---
+++ src/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+    public int creditScore;
+    public String state;
+
+    public Person(int creditScore, String state) {
+        this.creditScore = creditScore;
+        this.state = state;
+    }
+}
---
+++ src/Runner.java
@@ -0,0 +1,8 @@
+public class Runner {
+    public static void main(String[] args) {
+        Person yari = new Person(720, "Florida");
+        Product product = new Product("7-1 ARM", 5.0);
+        RulesEngine rulesEngine = new RulesEngine();
+
+    }
+}
